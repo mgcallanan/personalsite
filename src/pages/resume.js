b7c200d9b0fd5228274b142f107cfcacbc8fdba4
mgcallanan <marygcallanan@gmail.com>
2020-11-13 13:47:12 -0500

    Update styling according to designer reccommendation

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -24,53 +24,6 @@ export default function Resume() {
       <div className="general-page-content">
         <div className="section">
           <div className="section-header">
-            <h2>EDUCATION</h2>
-          </div>
-          <div className="section-content">
-            <div className="experience-main">
-              <div className="experience-logo">
-                <img src={yalelogo} alt="Yale Logo"/>
-              </div>
-              <div className="experience-main-text">
-                <div className="experience-title">
-                  <h3>Yale University</h3>
-                </div>
-                <div className="experience-date">
-                  <p>August 2018 - Present</p>
-                </div>
-              </div>
-            </div>
-            <div className="experience-description">
-              <ul>
-                <li><span>Bachelor of Science, Computer Science</span></li>
-                <li><span style={{fontStyle: "italic"}}>Relevant Classes:</span> Data Structures and Programming, Systems Programming & Computer Organization</li>
-              </ul>
-            </div>
-          </div>
-          <div className="section-content">
-            <div className="experience-main">
-              <div className="experience-logo">
-                <img src={dshalogo} alt="Divine Savior Holy Angels High School Seal"/>
-              </div>
-              <div className="experience-main-text">
-                <div className="experience-title">
-                  <h3 className="dsha">Divine Savior Holy Angels High School</h3>
-                </div>
-                <div className="experience-date">
-                  <p>August 2014 - May 2018</p>
-                </div>
-              </div>
-            </div>
-            <div className="experience-description">
-              <ul>
-                <li>Graduated in top 10% of the graduating class</li>
-                <li><span style={{fontStyle: "italic"}}>Awards:</span> National Honor Society, AP Scholar with Distinction, Salvatorian Sisters' Service Award</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="section">
-          <div className="section-header">
             <h2>WORK EXPERIENCE</h2>
           </div>
           <div className="section-content">
@@ -151,6 +104,53 @@ export default function Resume() {
                   <li>Host annual events to train area teachers on computer science principles and to showcase students’ final projects</li>
                 </ul>
               </div>
+            </div>
+          </div>
+        </div>
+        <div className="section">
+          <div className="section-header">
+            <h2>EDUCATION</h2>
+          </div>
+          <div className="section-content education">
+            <div className="experience-main">
+              <div className="experience-logo">
+                <img src={yalelogo} alt="Yale Logo"/>
+              </div>
+              <div className="experience-main-text">
+                <div className="experience-title">
+                  <h3>Yale University</h3>
+                </div>
+                <div className="experience-date">
+                  <p>August 2018 - Present</p>
+                </div>
+              </div>
+            </div>
+            <div className="experience-description">
+              <ul>
+                <li><span>Bachelor of Science, Computer Science</span></li>
+                <li><span style={{fontStyle: "italic"}}>Relevant Classes:</span> Data Structures and Programming, Systems Programming & Computer Organization</li>
+              </ul>
+            </div>
+          </div>
+          <div className="section-content education">
+            <div className="experience-main">
+              <div className="experience-logo">
+                <img src={dshalogo} alt="Divine Savior Holy Angels High School Seal"/>
+              </div>
+              <div className="experience-main-text">
+                <div className="experience-title">
+                  <h3 className="dsha">Divine Savior Holy Angels High School</h3>
+                </div>
+                <div className="experience-date">
+                  <p>August 2014 - May 2018</p>
+                </div>
+              </div>
+            </div>
+            <div className="experience-description">
+              <ul>
+                <li>Graduated in top 10% of the graduating class</li>
+                <li><span style={{fontStyle: "italic"}}>Awards:</span> National Honor Society, AP Scholar with Distinction, Salvatorian Sisters' Service Award</li>
+              </ul>
             </div>
           </div>
         </div>
