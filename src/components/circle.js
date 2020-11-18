617cc6d6d7d7b4f341cc1cdab00479b46181b05a
mgcallanan <marygcallanan@gmail.com>
2020-11-18 17:05:48 -0500

    Upload d3.js files

--- src/components/circle.js
+++ src/components/circle.js
@@ -12,31 +12,32 @@ export default function Circles () {
         ]))
     )
 
-        const [dataset, setDataset] = useState(
-          generateDataset()
-        )
-        const ref = useRef()
-      
-        useEffect(() => {
-          const svgElement = d3.select(ref.current)
-          svgElement.selectAll("circle")
-            .data(dataset)
-            .join("circle")
-              .attr("cx", d => d[0])
-              .attr("cy", d => d[1])
-              .attr("r",  3)
-        }, [dataset])
-      
-        useInterval(() => {
-          const newDataset = generateDataset()
-          setDataset(newDataset)
-        }, 2000)
-      
-        return (
-          <svg
-            viewBox="0 0 100 50"
-            ref={ref}
-          />
-        )
-      }
+    const [dataset, setDataset] = useState(
+        generateDataset()
+    )
+    const ref = useRef()
+    
+    useEffect(() => {
+        const svgElement = d3.select(ref.current)
+        svgElement.selectAll("circle")
+        .data(dataset)
+        .join("circle")
+            .attr("cx", d => d[0])
+            .attr("cy", d => d[1])
+            .attr("r",  3)
+            .attr("fill", "white")
+    }, [dataset])
+    
+    /*useInterval(() => {
+        const newDataset = generateDataset()
+        setDataset(newDataset)
+    }, 2000)*/
+    
+    return (
+        <svg
+        viewBox="0 0 100 50"
+        ref={ref}
+        />
+    )
+}
 
