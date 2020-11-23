061e82030b68751e46e815fe2bba7f88e1368800
mgcallanan <marygcallanan@gmail.com>
2020-11-23 16:06:42 -0500

    Revert to non-slideshow format for Portfolio

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,8 +2,6 @@ import React from "react"
 
 import Header from "../components/header"
 import Navigation from "../components/navigation"
-import Circles from "../components/circle"
-import Slideshow from "../components/slideshow"
 
 import "../styles/portfolio.css"
 
@@ -20,7 +18,35 @@ export default function Portfolio() {
         <Navigation/>
       </div>
       <div className="content">
-        <Slideshow/> 
+      <div className="portfolio-section">
+          <p className="section-title">&nbsp;Work from CPSC 446: Data Visualization&nbsp;</p>
+
+          <div class="slideshow-container">
+            
+            <div className="mySlides film">
+              <iframe className="d3-sample" src="/genderinmovies.html"></iframe>
+              <div className="caption">A visual representation of the gender divide in Hollywood. The blue in all of the visualizations represents male figures, and the red represents female figures. The data was scraped from <a href="https://github.com/taubergm/HollywoodGenderData">https://github.com/taubergm/HollywoodGenderData</a></div>
+            </div>
+
+            <div className="mySlides supreme">
+              <iframe className="d3-sample" src="/court.html"></iframe>
+              <div className="caption">A graphic showing the connections between various cases and the Supreme Court Justices who voted "Yes" on those particular cases.</div>
+            </div>
+
+            <div className="mySlides bars">
+              <iframe className="d3-sample" src="/looped_bars.html"></iframe>
+              <div className="caption">An animation with bar graphs showing arbitrary data in various formats. Click within the white frame to rotate views.</div>
+            </div>
+
+            <div className="mySlides map">
+              <iframe className="d3-sample" src="/genderdata.html"></iframe>
+              <div className="caption">A map depicting the pay gap between males and females in various US states from 2010.</div>
+            </div>
+
+          </div>
+          <br/>
+
+        </div>
       </div>
     </React.Fragment>
 
