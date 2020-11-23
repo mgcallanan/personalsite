1673525f28994283ccebb44b80344487ccbcdd58
mgcallanan <marygcallanan@gmail.com>
2020-11-23 16:46:47 -0500

    Add publication content

--- src/pages/publications.js
+++ src/pages/publications.js
@@ -3,6 +3,11 @@ import React from "react"
 import Header from "../components/header"
 import Navigation from "../components/navigation"
 
+import enasImg from "../images/enas118.jpg"
+import chImg from "../images/codehavenblog.png"
+
+import "../styles/publications.css"
+
 export default function Publications() {
 
   return (
@@ -15,6 +20,36 @@ export default function Publications() {
       </div>
       <div className="content">
         <p>Publications</p>
+        <a href="https://seas.yale.edu/news-events/news/enas-118-artful-innovations-carry-weight-and-make-surgery-go-easier">
+          <div className="publication-section">
+            <div className="thumbnail">
+              <img src={enasImg}/>
+            </div>
+            <div className="article-details">
+              <div className="article-name">
+                <h2>Artful Innovations that Carry Weight (and Make Surgery Go Easier)</h2>
+              </div>
+              <div className="article-date">
+                <p>January 18, 2019</p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="https://medium.com/code-haven/fostering-community-in-a-virtual-world-680306bb15">
+          <div className="publication-section">
+            <div className="thumbnail">
+              <img src={chImg}/>
+            </div>
+            <div className="article-details">
+              <div className="article-name">
+                <h2>Fostering Community in a Virtual World</h2>
+              </div>
+              <div className="article-date">
+                <p>October 3, 2020</p>
+              </div>
+            </div>
+          </div>
+        </a>
       </div>
     </React.Fragment>
 
