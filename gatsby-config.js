3e450cc3190f8bbbb187ab9e002726a62958af2d
mgcallanan <marygcallanan@gmail.com>
2020-11-24 22:12:19 -0500

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,22 +8,12 @@ module.exports = {
   /* Your site config here */
   plugins: [`gatsby-plugin-styled-components`, 
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "G-B5YDNJS4B8",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: [""],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Defers execution of google analytics script after page load
-        defer: false
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-B5YDNJS4B8", // Google Analytics / GA
+        ],
       },
     }
   ],
