62e0c1ec65e7d9cc9efc22d68d056a860b33f42a
mgcallanan <marygcallanan@gmail.com>
2025-11-05 21:59:37 -0500

    Fix hamburger

--- src/components/hamburger.tsx
+++ src/components/hamburger.tsx
@@ -27,30 +27,32 @@ const StyledButton = styled.button.attrs<StyledButtonProps>(({ onClick }) => ({
   border: none;
   cursor: pointer;
   z-index: 5;
+  padding: 0;
 
   div {
     width: 2rem;
     height: 0.2rem;
-    background: #bfbfbf;
-    border-radius: 5px;
-    transform-origin: 1px;
+    background: #ffffff;
+    border-radius: 4px;
+    transform-origin: center;
     position: relative;
-    margin-left: auto;
-    margin-right: auto;
-    transition: opacity 300ms, transform 300ms;
+    margin: 0 auto;
+    transition: all 300ms ease;
 
     :first-child {
-      transform: ${({ nav }: { nav: boolean }) =>
-        nav ? "rotate(45deg)" : "rotate(0)"};
+      transform: ${({ nav }) =>
+        nav ? "translateY(0.7rem) rotate(45deg)" : "rotate(0)"};
     }
 
     :nth-child(2) {
-      opacity: ${({ nav }: { nav: boolean }) => (nav ? "0" : "1")};
+      opacity: ${({ nav }) => (nav ? "0" : "1")};
+      transform: ${({ nav }) =>
+        nav ? "translateX(100%)" : "translateX(0)"};
     }
 
     :nth-child(3) {
-      transform: ${({ nav }: { nav: boolean }) =>
-        nav ? "rotate(-45deg)" : "rotate(0)"};
+      transform: ${({ nav }) =>
+        nav ? "translateY(-0.7rem) rotate(-45deg)" : "rotate(0)"};
     }
   }
 `
@@ -65,23 +67,26 @@ const StyledNav = styled.nav.attrs<StyledNavProps>(({ className }) => ({
   text-align: center;
   height: 100vh;
   width: 100%;
-  background: #636363;
+  background: rgba(0, 0, 0, 0.95);
   position: fixed;
   top: 0;
   right: 0;
-  transition: transform 300ms;
-  transform: ${({ nav }: { nav: boolean }) =>
+  transition: transform 300ms ease;
+  transform: ${({ nav }) =>
     nav ? "translateX(0)" : "translateX(100%)"};
   z-index: 4;
 
   ul {
     list-style-type: none;
-    vertical-align: center;
-    horizontal-align: center;
+    padding: 0;
+    margin: 0;
   }
 
   li {
-    margin-top: 2rem;
+    margin: 2rem 0;
+    opacity: ${({ nav }) => (nav ? "1" : "0")};
+    transition: opacity 300ms ease;
+    transition-delay: ${({ nav }) => (nav ? "200ms" : "0")};
   }
 
   a {
@@ -89,12 +94,16 @@ const StyledNav = styled.nav.attrs<StyledNavProps>(({ className }) => ({
     color: white;
     font-size: 1.5rem;
     font-family: acumin-pro-wide, sans-serif;
-    font-weight: 200;
-    font-style: normal;
-    transition: color 300ms;
+    font-weight: 300;
+    letter-spacing: 0.1em;
+    transition: color 200ms ease;
 
-    :hover {
+    &:hover {
       color: #6ab4ff;
+    }
+
+    p {
+      margin: 0;
     }
   }
 `
@@ -107,13 +116,18 @@ interface HamburgerProps {
 const Hamburger: React.FC<HamburgerProps> = ({ isOpen = false, onToggle }) => {
   return (
     <div className="hamburger">
-      <StyledButton nav={isOpen} onClick={onToggle}>
+      <StyledButton nav={isOpen} onClick={onToggle} aria-label="Menu">
         <div />
         <div />
         <div />
       </StyledButton>
       <StyledNav nav={isOpen} className="hamburger_links">
         <ul>
+          <li>
+            <Link to="/">
+              <p>HOME</p>
+            </Link>
+          </li>
           <li>
             <Link to="/resume/">
               <p>RESUME</p>
@@ -129,11 +143,6 @@ const Hamburger: React.FC<HamburgerProps> = ({ isOpen = false, onToggle }) => {
               <p>PUBLICATIONS</p>
             </Link>
           </li>
-          {/* <li>
-                        <Link to="/justforfun/">
-                            <p>JUST FOR FUN</p>
-                        </Link>
-                    </li> */}
         </ul>
       </StyledNav>
     </div>
